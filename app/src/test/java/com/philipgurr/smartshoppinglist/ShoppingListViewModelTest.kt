b841dee541ca58b2f6420061ea9ffa719550df55
Philip Gurr <philipgurr10@gmail.com>
2019-11-16 16:56:08 +0100

    Create test

---
+++ app/src/test/java/com/philipgurr/smartshoppinglist/ShoppingListViewModelTest.kt
@@ -0,0 +1,40 @@
+package com.philipgurr.smartshoppinglist
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.philipgurr.smartshoppinglist.domain.Product
+import com.philipgurr.smartshoppinglist.domain.ShoppingList
+import com.philipgurr.smartshoppinglist.repository.Repository
+import com.philipgurr.smartshoppinglist.vm.ShoppingListViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class ShoppingListViewModelTest {
+
+    @get:Rule
+    val instantTaskRule = InstantTaskExecutorRule()
+
+    private val repository: Repository<ShoppingList> = mock()
+
+    private val testProduct = Product("Banana", false)
+    private val testProduct2 = Product("Strawberry Jam", true)
+    private val testShoppingList = ShoppingList(
+        "testShoppingList",
+        listOf(testProduct, testProduct2)
+    )
+
+    @Before
+    fun before() {
+        whenever(repository.getAll()).thenReturn(listOf(testShoppingList))
+    }
+
+    @Test
+    fun testShoppingViewModel() {
+        val viewModel = ShoppingListViewModel(repository)
+        viewModel.loadShoppingLists()
+        assertEquals(listOf(testShoppingList), viewModel.shoppingList.value)
+    }
+}
