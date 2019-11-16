aa53ed7f05f92f9adbe7f4c76b4431ffd712ad3d
Philip Gurr <philipgurr10@gmail.com>
2019-11-16 17:37:50 +0100

    Minor syntax changes

--- app/src/main/java/com/philipgurr/smartshoppinglist/domain/ShoppingList.kt
+++ app/src/main/java/com/philipgurr/smartshoppinglist/domain/ShoppingList.kt
@@ -1,7 +1,5 @@
 package com.philipgurr.smartshoppinglist.domain
 
-import android.app.ProgressDialog
-
-class ShoppingList(val name: String, val items: List<Product>) {
-    fun getCompletedProducts() = items.filter { it.completed }
+class ShoppingList(val name: String, val products: List<Product>) {
+    fun getCompletedProducts() = products.filter { it.completed }
 }
