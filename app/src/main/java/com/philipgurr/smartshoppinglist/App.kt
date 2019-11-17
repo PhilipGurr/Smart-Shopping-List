607d1f807b62f32d90b3a7298162dee5e26bf514
Philip Gurr <philipgurr10@gmail.com>
2019-11-17 21:31:27 +0100

    Initialize firebase

--- app/src/main/java/com/philipgurr/smartshoppinglist/App.kt
+++ app/src/main/java/com/philipgurr/smartshoppinglist/App.kt
@@ -1,10 +1,17 @@
 package com.philipgurr.smartshoppinglist
 
+import com.google.firebase.FirebaseApp
 import com.philipgurr.smartshoppinglist.di.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 class App : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerApplicationComponent
             .factory()
