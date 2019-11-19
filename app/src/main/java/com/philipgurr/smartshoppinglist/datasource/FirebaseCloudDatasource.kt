822846576672894df964466ab0a1b198727f6de3
Philip Gurr <philipgurr10@gmail.com>
2019-11-19 23:39:30 +0100

    Refactoring

--- app/src/main/java/com/philipgurr/smartshoppinglist/datasource/FirebaseCloudDatasource.kt
+++ app/src/main/java/com/philipgurr/smartshoppinglist/datasource/FirebaseCloudDatasource.kt
@@ -1,39 +1,51 @@
 package com.philipgurr.smartshoppinglist.datasource
 
-import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.philipgurr.smartshoppinglist.domain.Product
 import com.philipgurr.smartshoppinglist.domain.ShoppingList
 import com.philipgurr.smartshoppinglist.util.extensions.await
+import com.philipgurr.smartshoppinglist.util.extensions.parse
 import javax.inject.Inject
 
+private const val USER_LISTS_COLLECTION_ID = "user"
+private const val SHOPPING_LISTS_COLLECTION_ID = "shoppinglists"
+private const val PRODUCT_LISTS_COLLECTION_ID = "products"
+
 class FirebaseCloudDatasource @Inject constructor() : Datasource<ShoppingList> {
-
     private val database = Firebase.firestore
+    private val shoppingListCollection = database
+        .collection(USER_LISTS_COLLECTION_ID)
+        .document("test")
+        .collection(SHOPPING_LISTS_COLLECTION_ID)
 
-    override suspend fun get(name: String): ShoppingList {
-        val shoppingListDocument = getShoppingListDocument(name)
-        return parseDocument(shoppingListDocument)
+    override suspend fun get(name: String) = getShoppingList(name)
+
+    private suspend fun getShoppingList(name: String): ShoppingList {
+        val snapshot = shoppingListCollection.document(name).get().await()
+        return snapshot.parse<ShoppingList>()!!.apply {
+            id = snapshot.id
+            products = getProducts(snapshot.id)
+        }
     }
 
-    private fun getShoppingListDocument(name: String) = database
-            .collection("user")
-            .document("test")
-            .collection("shoppinglists")
-            .document(name)
-
-    private suspend fun parseDocument(document: DocumentReference): ShoppingList {
-        val documentSnapshot = document.get().await()
-        return documentSnapshot.toObject(ShoppingList::class.java)!!
-    }
+    private suspend fun getProducts(shoppingListId: String) =
+        shoppingListCollection
+            .document(shoppingListId)
+            .collection(PRODUCT_LISTS_COLLECTION_ID)
+            .get().await()
+            .parse<Product>()
 
     override suspend fun getAll(): List<ShoppingList> {
-        val snapshots = database
-            .collection("user")
-            .document("test")
-            .collection("shoppinglists")
-            .get().await()
-        return snapshots.toObjects(ShoppingList::class.java)
+        val snapshots = shoppingListCollection.get().await()
+        val shoppingLists = mutableListOf<ShoppingList>()
+
+        for (snapshot in snapshots) {
+            val shoppingListId = snapshot.id
+            shoppingLists.add(getShoppingList(shoppingListId))
+        }
+
+        return shoppingLists
     }
 
     override suspend fun insert(value: ShoppingList) {
@@ -43,5 +55,4 @@ class FirebaseCloudDatasource @Inject constructor() : Datasource<ShoppingList> {
     override suspend fun insertAll(values: List<ShoppingList>) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-
 }
--- app/src/main/java/com/philipgurr/smartshoppinglist/util/ProgressBarColorPicker.kt
+++ app/src/main/java/com/philipgurr/smartshoppinglist/util/ProgressBarColorPicker.kt
@@ -4,16 +4,19 @@ import com.philipgurr.smartshoppinglist.R
 
 object ProgressBarColorPicker {
 
+    private const val LOWER_BOUND = 30
+    private const val UPPER_BOUND = 100
+
     fun choose(progress: Int, total: Int): Int {
+
         val percent = calculatePercentage(progress, total)
 
         return when {
-            percent < 30 -> R.color.red
-            percent in 30 until 100 -> R.color.orange
+            percent < LOWER_BOUND -> R.color.red
+            percent in LOWER_BOUND until UPPER_BOUND -> R.color.orange
             else -> R.color.green
         }
     }
 
-    private fun calculatePercentage(progress: Int, total: Int) =
-        (progress * 100.0f / total).toInt()
+    private fun calculatePercentage(progress: Int, total: Int) = (progress * 100.0f / total).toInt()
 }
