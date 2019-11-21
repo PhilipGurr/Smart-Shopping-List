ba3ab188e25efd31786083a5425164776caf8706
Philip Gurr <philipgurr10@gmail.com>
2019-11-21 17:06:13 +0100

    Implement Serializable

--- app/src/main/java/com/philipgurr/smartshoppinglist/domain/ShoppingList.kt
+++ app/src/main/java/com/philipgurr/smartshoppinglist/domain/ShoppingList.kt
@@ -1,9 +1,11 @@
 package com.philipgurr.smartshoppinglist.domain
 
+import java.io.Serializable
+
 class ShoppingList(
     var id: String = "",
     val name: String = "",
     var products: List<Product> = listOf()
-) {
+) : Serializable {
     fun getCompletedProducts() = products.filter { it.isCompleted }
 }
