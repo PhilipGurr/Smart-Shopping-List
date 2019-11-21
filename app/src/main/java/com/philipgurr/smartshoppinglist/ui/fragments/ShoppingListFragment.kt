b34555267859f6d7a5679d7ba965d8ae51773af3
Philip Gurr <philipgurr10@gmail.com>
2019-11-21 17:27:06 +0100

    Display shopping lists

--- app/src/main/java/com/philipgurr/smartshoppinglist/ui/fragments/ShoppingListFragment.kt
+++ app/src/main/java/com/philipgurr/smartshoppinglist/ui/fragments/ShoppingListFragment.kt
@@ -1,37 +1,59 @@
 package com.philipgurr.smartshoppinglist.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.philipgurr.smartshoppinglist.R
+import com.philipgurr.smartshoppinglist.domain.ShoppingList
+import com.philipgurr.smartshoppinglist.ui.ShoppingListAdapter
 import com.philipgurr.smartshoppinglist.vm.ShoppingListViewModel
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_my_lists.*
 import javax.inject.Inject
 
 class ShoppingListFragment : DaggerFragment() {
-
     @Inject
     lateinit var factory: ViewModelProvider.Factory
     private val viewModel by lazy {
         ViewModelProviders.of(this, factory).get(ShoppingListViewModel::class.java)
     }
 
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private val shoppingListAdapter = ShoppingListAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_my_lists, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        viewModel.shoppingList.observe(this, Observer {
-            textView.text = "Sample text"
-        })
+        return inflater.inflate(R.layout.fragment_my_lists, container, false)
+    }
 
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+
+        viewModel.shoppingLists.observe(this, Observer { shoppingLists ->
+            shoppingListAdapter.data = shoppingLists
+            shoppingListAdapter.notifyDataSetChanged()
+        })
+    }
+
+    private fun setupRecyclerView() {
+        linearLayoutManager = LinearLayoutManager(context)
+        myListsRecyclerView.layoutManager = linearLayoutManager
+        myListsRecyclerView.adapter = shoppingListAdapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.loadShoppingLists()
     }
 }
