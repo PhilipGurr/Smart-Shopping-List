023874f4f91a94ae271751dcf0a29eb12e819c73
Philip Gurr <philipgurr10@gmail.com>
2019-11-23 14:36:18 +0100

    Switch to sorting by creation date

--- app/src/main/java/com/philipgurr/smartshoppinglist/domain/ShoppingList.kt
+++ app/src/main/java/com/philipgurr/smartshoppinglist/domain/ShoppingList.kt
@@ -1,10 +1,12 @@
 package com.philipgurr.smartshoppinglist.domain
 
 import java.io.Serializable
+import java.util.*
 
 class ShoppingList(
     var id: String = "",
     val name: String = "",
+    val created: Date = Date(),
     var products: List<Product> = listOf()
 ) : Serializable {
     fun getCompletedProducts() = products.filter { it.completed }
--- app/src/main/java/com/philipgurr/smartshoppinglist/domain/usecases/GetShoppingListsUseCase.kt
+++ app/src/main/java/com/philipgurr/smartshoppinglist/domain/usecases/GetShoppingListsUseCase.kt
@@ -2,11 +2,15 @@ package com.philipgurr.smartshoppinglist.domain.usecases
 
 import com.philipgurr.smartshoppinglist.domain.ShoppingList
 import com.philipgurr.smartshoppinglist.repository.Repository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetShoppingListsUseCase @Inject constructor(
     private val repository: Repository<ShoppingList>
 ) {
 
-    suspend fun getShoppingLists() = repository.getAll()
+    suspend fun getShoppingLists() = withContext(Dispatchers.Default) {
+        repository.getAll().sortedBy { it.created }
+    }
 }
