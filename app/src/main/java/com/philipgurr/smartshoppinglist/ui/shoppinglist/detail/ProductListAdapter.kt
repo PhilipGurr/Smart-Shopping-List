0a8d992de849c455bad10c2a7a37bcf14ad59fbc
Philip Gurr <philipgurr10@gmail.com>
2019-11-27 20:19:23 +0100

    Create base Adapter for Data Binding

--- app/src/main/java/com/philipgurr/smartshoppinglist/ui/shoppinglist/detail/ProductListAdapter.kt
+++ app/src/main/java/com/philipgurr/smartshoppinglist/ui/shoppinglist/detail/ProductListAdapter.kt
@@ -8,34 +8,25 @@ import androidx.recyclerview.widget.RecyclerView
 import com.philipgurr.smartshoppinglist.R
 import com.philipgurr.smartshoppinglist.databinding.ProductlistItemBinding
 import com.philipgurr.smartshoppinglist.domain.Product
+import com.philipgurr.smartshoppinglist.ui.ListBindingAdapter
 
-class ProductListAdapter : RecyclerView.Adapter<ProductListAdapter.ItemViewHolder>() {
+class ProductListAdapter : ListBindingAdapter<Product, ProductlistItemBinding>() {
+    override var data = listOf<Product>()
 
-    var data = listOf<Product>()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
+    override fun createViewHolder(
+        inflater: LayoutInflater,
+        parent: ViewGroup
+    ): ItemViewHolder<Product, ProductlistItemBinding> {
         val binding: ProductlistItemBinding =
             DataBindingUtil.inflate(inflater, R.layout.productlist_item, parent, false)
-        return ItemViewHolder(binding)
+        return ProductViewHolder(binding)
     }
 
-    override fun getItemCount() = data.size
+    class ProductViewHolder(private val binding: ProductlistItemBinding) :
+        ItemViewHolder<Product, ProductlistItemBinding>(binding) {
 
-    override fun onBindViewHolder(holderItem: ItemViewHolder, position: Int) {
-        holderItem.bind(data[position])
-    }
-
-    private fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false) =
-        LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
-
-    class ItemViewHolder(private val binding: ProductlistItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(product: Product) {
-            binding.product = product
+        override fun bind(item: Product) {
+            binding.product = item
         }
-
-        // TODO: Implement OnClick logic
     }
 }
--- app/src/main/java/com/philipgurr/smartshoppinglist/ui/shoppinglist/main/ShoppingListAdapter.kt
+++ app/src/main/java/com/philipgurr/smartshoppinglist/ui/shoppinglist/main/ShoppingListAdapter.kt
@@ -3,40 +3,34 @@ package com.philipgurr.smartshoppinglist.ui.shoppinglist.main
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.Navigation
-import androidx.recyclerview.widget.RecyclerView
 import com.philipgurr.smartshoppinglist.databinding.ShoppinglistItemBinding
 import com.philipgurr.smartshoppinglist.domain.ShoppingList
+import com.philipgurr.smartshoppinglist.ui.ListBindingAdapter
 import com.philipgurr.smartshoppinglist.ui.shoppinglist.ShoppingListToUIMapper
-import com.philipgurr.smartshoppinglist.ui.shoppinglist.main.ShoppingListFragmentDirections
 
-class ShoppingListAdapter : RecyclerView.Adapter<ShoppingListAdapter.ItemViewHolder>() {
-    var data = listOf<ShoppingList>()
+class ShoppingListAdapter : ListBindingAdapter<ShoppingList, ShoppinglistItemBinding>() {
+    override var data = listOf<ShoppingList>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
+    override fun createViewHolder(
+        inflater: LayoutInflater,
+        parent: ViewGroup
+    ): ItemViewHolder<ShoppingList, ShoppinglistItemBinding> {
         val binding = ShoppinglistItemBinding.inflate(inflater, parent, false)
-        val viewHolder =
-            ItemViewHolder(
-                binding
-            )
-        binding.viewHolder = viewHolder
-        return viewHolder
+        return ShoppingListViewHolder(binding)
     }
 
-    override fun getItemCount() = data.size
-
-    override fun onBindViewHolder(holderItem: ItemViewHolder, position: Int) {
-        holderItem.bind(data[position])
-    }
-
-    class ItemViewHolder(private val binding: ShoppinglistItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ShoppingListViewHolder(private val binding: ShoppinglistItemBinding) :
+        ItemViewHolder<ShoppingList, ShoppinglistItemBinding>(binding) {
         private val mapper = ShoppingListToUIMapper()
 
-        fun bind(shoppingList: ShoppingList) {
-            val shoppingListUI = mapper.map(shoppingList)
-            binding.shoppingList = shoppingList
-            binding.shoppingListUI = shoppingListUI
-            binding.executePendingBindings()
+        override fun bind(item: ShoppingList) {
+            val uiModel = mapper.map(item)
+            with(binding) {
+                viewHolder = this@ShoppingListViewHolder
+                shoppingList = item
+                shoppingListUI = uiModel
+                executePendingBindings()
+            }
         }
 
         fun navigateToDetailScreen(shoppingList: ShoppingList) {
