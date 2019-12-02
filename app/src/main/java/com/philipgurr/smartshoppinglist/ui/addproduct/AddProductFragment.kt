e4bc75bec6ad9576e055835f0c138660b2a51566
Philip Gurr <philipgurr10@gmail.com>
2019-12-02 19:24:27 +0100

    Minor syntax change

--- app/src/main/java/com/philipgurr/smartshoppinglist/ui/addproduct/AddProductFragment.kt
+++ app/src/main/java/com/philipgurr/smartshoppinglist/ui/addproduct/AddProductFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.philipgurr.smartshoppinglist.R
 import kotlinx.android.synthetic.main.fragment_add_product.*
 
