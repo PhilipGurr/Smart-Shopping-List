88b437d908ba438fba1e7da48deee38fe9804c24
Philip Gurr <philipgurr10@gmail.com>
2019-12-26 23:24:26 +0100

    Implement login with Firebase Auth and Google Sign-In

--- app/src/main/java/com/philipgurr/smartshoppinglist/ui/MainActivity.kt
+++ app/src/main/java/com/philipgurr/smartshoppinglist/ui/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.philipgurr.smartshoppinglist.ui
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
@@ -9,9 +11,17 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationView
+import coil.api.load
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.philipgurr.smartshoppinglist.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_header_main.view.*
+import kotlinx.android.synthetic.main.nav_header_not_logged_in.view.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
+
+const val RC_SIGN_IN = 1001
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,14 +41,83 @@ class MainActivity : AppCompatActivity() {
 
     private fun createAndShowNavigation() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
+        setupNavigationHeader()
+
+        nav_view.setNavigationItemSelectedListener { item ->
+            when (val id = item.itemId) {
+                R.id.nav_signout -> {
+                    FirebaseAuth.getInstance().signOut()
+                    showUserNotLoggedIn()
+                }
+                else -> navController.navigate(id)
+            }
+            true
+        }
 
         appBarConfiguration = AppBarConfiguration(
             setOf(R.id.nav_shopping_lists), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+    }
+
+    private fun setupNavigationHeader() {
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            showUserDetails(user)
+        } else {
+            showUserNotLoggedIn()
+        }
+    }
+
+    private fun showUserDetails(user: FirebaseUser?) {
+        removeNavigationHeader()
+        val header = nav_view.inflateHeaderView(R.layout.nav_header_main)
+        user?.let {
+            header.userName.text = user.displayName
+            header.email.text = user.email
+            header.userPic.load(user.photoUrl)
+        }
+    }
+
+    private fun showUserNotLoggedIn() {
+        removeNavigationHeader()
+        val header = nav_view.inflateHeaderView(R.layout.nav_header_not_logged_in)
+        val loginButton = header.loginButton
+        loginButton.onClick {
+            login()
+        }
+    }
+
+    fun removeNavigationHeader() {
+        if (nav_view.headerCount > 0) {
+            nav_view.removeHeaderView(nav_view.getHeaderView(0))
+        }
+    }
+
+    private fun login() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+                showUserDetails(user)
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
