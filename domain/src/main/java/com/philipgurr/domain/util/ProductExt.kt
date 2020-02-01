25a8cef4158ff713fc8f69699ba0b139f76f033f
Philip Gurr <philipgurr10@gmail.com>
2020-02-01 01:50:30 +0100

    Fix data binding error

--- domain/src/main/java/com/philipgurr/domain/util/ProductExt.kt
+++ domain/src/main/java/com/philipgurr/domain/util/ProductExt.kt
@@ -4,7 +4,7 @@ import com.philipgurr.domain.Product
 
 private const val MAX_WORDS_IN_NAME = 4
 
-fun Product.displayName(): String {
-    val words = name.split(" ")
+fun Product?.displayName(): String {
+    val words = this?.name?.split(" ") ?: listOf()
     return words.joinToString(limit = MAX_WORDS_IN_NAME, separator = " ", truncated = "").trim()
 }
